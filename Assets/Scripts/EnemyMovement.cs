629e19b8663856e458248c00d88890338cfa2dec
Enemy-Stand <sestijoey63@gmail.com>
2023-07-03 12:20:34 -0500

    got enemy flip working without weird shit happening

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,16 +7,31 @@ public class EnemyMovement : MonoBehaviour
 
     Rigidbody2D myRigidbody;
     Animator myAnimator;
+    BoxCollider2D wallDetection;
     [SerializeField] float moveSpeed = 1f;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        Debug.Log(transform.rotation.x);
+        //Debug.Log(transform.rotation.y);
+        //Debug.Log(transform.rotation.z);
     }
 
 
     void Update()
     {
         myRigidbody.velocity = new Vector2 (moveSpeed, 0f);
+    }
+
+    void FlipEnemy()
+    {
+        transform.localScale = new Vector2 (-(Mathf.Sign(myRigidbody.velocity.x)), 1f);
+    }
+    
+    void OnTriggerExit2D(Collider2D other)
+    {
+        moveSpeed = -moveSpeed;
+        FlipEnemy();
     }
 }
